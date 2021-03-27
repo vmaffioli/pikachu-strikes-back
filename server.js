a6039d8ce776d03bd8b5a5f24d259fd64545ed32
vmaffioli <vinimaffioli@gmail.com>
2021-03-27 04:37:09 -0300

    add node server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, './')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+
+
+
